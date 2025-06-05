c19aa97260c23ebe716fade8a042d08847fd893d
Hema sri Budida <bhemasri56@gmail.com>
2025-06-05 10:38:27 +0530

    first commit

---
+++ Assignment-questions/question_1.py
@@ -0,0 +1,6 @@
+input1 = int(input("Enter the first number: "))
+input2 = int(input("enter the second number: "))
+sum_of_the_inputs = input1+input2
+average_of_the_inputs = (input1+input2)/2
+print("Sum of the inputs:", sum_of_the_inputs)
+print("Average of the inputs:", average_of_the_inputs)
